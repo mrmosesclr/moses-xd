38c50818345f1846b376535d7a54b272a82facb4
ᴍʀ ᴍᴏsᴇs <marinyamestudios@gmail.com>
2025-07-06 14:58:47 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'MOSES-XD',
+  author: '‎',
+  botName: "MOSES-XD",
+  botOwner: 'Natty', // Your name
+  ownerNumber: '263787673035', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "A smart whatsApp bot.",
+  version: "3.8.0",
+};
+
+module.exports = settings;
